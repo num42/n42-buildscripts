16b793f97acad6ba2422690e92219687325d210e
Wolfgang Lutz <WLBORg@gmx.de>
2017-05-05 17:40:29 +0200

    Update Misspell.sh

--- iOS/Misspell.sh
+++ iOS/Misspell.sh
@@ -2,11 +2,11 @@
 SCRIPT_FILE="Misspell.sh"
 SCRIPT_SOURCE="https://raw.githubusercontent.com/num42/n42-buildscripts/master/iOS/${SCRIPT_FILE}"
 
-echo "Running N42 Misspell Script v1.00 (2017-05-05)"
+echo "Running N42 Misspell Script v1.01 (2017-05-05)"
 
 if [[ $1 == "-u" ]] ; then
   echo ""
-  echo  "$Updating ${SCRIPT_FILE}";
+  echo  "Updating ${SCRIPT_FILE}";
   curl -L $SCRIPT_SOURCE?$(date +%s) -o $0
   exit 1
 fi
