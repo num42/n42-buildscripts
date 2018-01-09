8c9a4e05d3b2fd61e3fbf6e469ef9a7882c7ea61
Wolfgang Lutz <wlut@num42.de>
2018-01-09 11:00:15 +0100

    remove threading and slicing
    this no longer brings an increase in speed

--- iOS/EmbedCarthageFrameworks.rb
+++ iOS/EmbedCarthageFrameworks.rb
@@ -3,7 +3,7 @@
 require 'optparse'
 require 'ostruct'
 
-version = "0.0.4"
+version = "0.0.5"
 
 scriptFile = "EmbedCarthageFrameworks.rb"
 scriptSource = "https://raw.githubusercontent.com/num42/n42-buildscripts/master/iOS/#{scriptFile}"
@@ -12,22 +12,22 @@ options = OpenStruct.new
 
 options.frameworkFileName = nil
 options.update = false
-    
+
 OptionParser.new do |opts|
-  
-  opts.on("-f", "--frameworks [Filename]", "Run 'carthage copy-frameworks' parallelized for all lines in this file") do |frameworkFileName|
+
+  opts.on("-f", "--frameworks [Filename]", "Run 'carthage copy-frameworks' for all lines in this file") do |frameworkFileName|
     options.frameworkFileName = frameworkFileName
   end
-  
+
   opts.on_tail("-h", "--help", "Show this message") do
       puts opts
       exit
   end
-    
+
   opts.on("-u", "--[no-]update", "Update Script") do |v|
     options.update = v
   end
-   
+
   opts.on_tail("--version", "Show version") do
      puts version
      exit
@@ -47,22 +47,14 @@ end
 
 frameworks = File.readlines "#{ENV["SRCROOT"]}/#{options.frameworkFileName}"
 
-threads = []
+hash = {}
 
-frameworks.each_slice(5) do | slice |
-    threads << Thread.new do
-        hash = {}
-        
-        slice.each_with_index do | framework, index |
-            hash["SCRIPT_INPUT_FILE_#{index}"] = "#{ENV["SRCROOT"]}/#{framework.strip}"
-            hash["SCRIPT_OUTPUT_FILE_#{index}"] = "#{ENV["BUILT_PRODUCTS_DIR"]}/#{ENV["FRAMEWORKS_FOLDER_PATH"]}/#{framework.strip}"
-        end
-        
-        puts hash
-        hash["SCRIPT_INPUT_FILE_COUNT"] = slice.count.to_s
-        
-        system(hash, "/usr/local/bin/carthage copy-frameworks")
-    end
+frameworks.each_with_index do | framework, index |
+    hash["SCRIPT_INPUT_FILE_#{index}"] = "#{ENV["SRCROOT"]}/#{framework.strip}"
+    hash["SCRIPT_OUTPUT_FILE_#{index}"] = "#{ENV["BUILT_PRODUCTS_DIR"]}/#{ENV["FRAMEWORKS_FOLDER_PATH"]}/#{framework.strip}"
 end
 
-threads.each { |thr| thr.join }
+puts hash
+hash["SCRIPT_INPUT_FILE_COUNT"] = frameworks.count.to_s
+
+system(hash, "/usr/local/bin/carthage copy-frameworks")
