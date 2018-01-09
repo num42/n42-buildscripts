a443106711b31399bc68aa3c7d7a7985b9e03639
Wolfgang Lutz <wlut@num42.de>
2018-01-09 11:05:53 +0100

    Add script for mint-backed run of swiflint and swiftformat

---
+++ iOS/AutocorrectAndFormat.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/ruby
+
+require 'optparse'
+require 'ostruct'
+require 'xcodeproj'
+
+version = "0.0.1"
+
+scriptFile = "AutocorrectAndFormat.rb"
+scriptSource = "https://raw.githubusercontent.com/num42/n42-buildscripts/master/iOS/#{scriptFile}"
+
+options = OpenStruct.new
+
+options.update = false
+
+OptionParser.new do |opts|
+
+opts.on_tail("-h", "--help", "Show this message") do
+puts opts
+exit
+end
+
+opts.on("-u", "--[no-]update", "Update Script") do |v|
+options.update = v
+end
+
+opts.on_tail("--version", "Show version") do
+puts version
+exit
+end
+
+end.parse!
+
+if options.update
+puts "updating"
+exec("curl -L #{scriptSource}?$(date +%s) -o #{File.basename($0)}")
+end
+
+# Here begins the actual script
+
+project = Xcodeproj::Project.open(ENV["PROJECT_FILE_PATH"])
+
+input_files = project.targets.select { |target| target.name.eql? ENV["TARGETNAME"] }.first.source_build_phase.files.to_a.map do |pbx_build_file|
+pbx_build_file.file_ref.real_path.to_s
+end.select do |path|
+path.end_with?(".m", ".mm", ".swift")
+end.select do |path|
+not path.include?("GeneratedCode")
+end
+
+hash = {}
+
+input_files.each_with_index do | filename, index |
+hash["SCRIPT_INPUT_FILE_#{index}"] = filename
+end
+
+hash["SCRIPT_INPUT_FILE_COUNT"] = input_files.count.to_s
+
+system(hash, "mint run realm/SwiftLint@$(cat .brew-versions | grep swiftlint | cut -d \":\" -f 2) \"swiftlint autocorrect \-\-use-script-input-files\"")
+
+system("swiftformat --disable redundantSelf --cache ignore --indent 2 --wraparguments beforefirst --wrapelements beforefirst --header ignore --patternlet inline --stripunusedargs closure-only --insertlines disabled --commas inline #{input_files.join(" ")}")
+
+system(hash, "mint run realm/SwiftLint@$(cat .brew-versions | grep swiftlint | cut -d \":\" -f 2) \"swiftlint lint \-\-quiet \-\-use-script-input-files\"")
