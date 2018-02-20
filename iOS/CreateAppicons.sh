ba461bce5c8dd3f8998f3e2ab3a28d5b953f5442
Wolfgang Lutz <wlut@num42.de>
2018-02-20 12:02:50 +0100

    CreateAppIcons: massive speedup

--- iOS/CreateAppicons.sh
+++ iOS/CreateAppicons.sh
@@ -3,7 +3,7 @@
 SCRIPT_FILE="CreateAppicons.sh"
 SCRIPT_SOURCE="https://raw.githubusercontent.com/num42/n42-buildscripts/master/iOS/${SCRIPT_FILE}"
 
-echo "Running AppiconScript v1.02 (9. January 2018)"
+echo "Running AppiconScript v1.1 (20. February 2018)"
 
 if [[ $1 == "-u" ]] ; then
     echo ""
@@ -43,9 +43,33 @@ esac
 
 BASE=`basename "$IMAGE_NAME"`
 
+if [ ! -d "${PROJECT_DIR}/build" ]; then
+  mkdir ${PROJECT_DIR}/build
+fi
+
+TMP_PATH=${PROJECT_DIR}/build
+
+move_if_different(){
+  if [ -e "$2" ]
+  then
+    #Compare the files at the paths given
+    compare -identify -metric MAE "$1" "$2" "$TMP_PATH/null"
+
+    RETVAL=$?
+
+    # If files are equal, delete first file
+    [ $RETVAL -eq 0 ] && rm "$1"
+
+    # If files are different, update second file
+    [ $RETVAL -ne 0 ] && echo "Copying" && mv "$1" "$2"
+  else
+    mv "$1" "$2"
+  fi
+}
+
 for SIZE in 20 29 40 48 50 55 57 58 60 72 76 80 87 100 114 120 144 152 167 172 180 196 1024; do
-# ensure alpha is off for itunes connect
-convert "$IMAGE_NAME" -resize $SIZEx$SIZE -alpha off "$APPICON_SET_PATH/$SIZE.png" &
+  # ensure alpha is off for itunes connect
+  convert "$IMAGE_NAME" -resize $SIZEx$SIZE -alpha off "$TMP_PATH/tmp_$SIZE.png"; move_if_different "$TMP_PATH/tmp_$SIZE.png" "$APPICON_SET_PATH/$SIZE.png" &
 done;
 
 wait
