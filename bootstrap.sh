3a240375a8177aea503f1b2535985a6c9a955788
Wolfgang Lutz <wlut@num42.de>
2018-03-26 10:53:43 +0200

    update version

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,7 @@ NOCOLOR=`tput sgr0`
 SCRIPT_FILE="bootstrap.sh"
 SCRIPT_SOURCE="https://raw.githubusercontent.com/num42/n42-buildscripts/master/${SCRIPT_FILE}"
 
-echo "${GREEN}Running N42 Bootstrap v1.20 (2018-02-26)${NOCOLOR}"
+echo "${GREEN}Running N42 Bootstrap v1.30 (2018-03-26)${NOCOLOR}"
 echo "${GREEN}If the script fails, there might be a newer Version on $SCRIPT_SOURCE ${NOCOLOR}"
 echo "${GREEN}You can directly download it with 'curl -L $SCRIPT_SOURCE -o ${SCRIPT_FILE}' ${NOCOLOR}"
 echo "${GREEN}You can update the script by running "sh ${SCRIPT_FILE} -u"' ${NOCOLOR}"
